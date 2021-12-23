c97125ac10d16444be8e7cb4e305a3fc8af8b89e
songerard <songerard@gmail.com>
2021-12-23 20:45:05 +0800

    feat: Add create and edit todo feature

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const users = require('./modules/users')
+const todos = require('./modules/todos')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
+router.use('/todos', authenticator, todos)
 router.use('/', authenticator, home)
 
 module.exports = router
